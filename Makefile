a14b74114ad6de6acceadfa9a86faab9bbbb37a9
Dylan Wang <wang78@llnl.gov>
2014-08-12 13:12:39 -0700

    Added new test for any-size MPI with counters

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
-# If make fails, make sure papi module is loaded!
+# If make fails, make sure papi module is loaded! Also try unloading darshan
 
 CFLAGS = -O0 -g -Wall
 LFLAGS = -lstdc++
-all: counter.out solo.out
+all: counter.out sample.out
 
 counter.out : test.c libariescounters.a
 	cc ${CFLAGS} -o counter.out test.c -L./libariescounters.a
 
-solo.out: test2.cpp libariescounters.a
-	cc ${CFLAGS} -o solo.out test2.cpp -L./libariescounters.a
+sample.out: test2.cpp libariescounters.a
+	cc ${CFLAGS} -o sample.out test2.cpp -L./libariescounters.a
+
+mpitest.out: test3.cpp libariescounters.a
+	cc ${CFLAGS} -o mpitest.out test3.cpp -L./libariescounters.a
 
 #libariescounters.a: AriesCounters.hpp AriesCounters.cpp
 #	CC ${CFLAGS} -c AriesCounters.cpp
@@ -19,5 +22,5 @@ libariescounters.a: AriesCounters.h
 	ar -cvq libariescounters.a AriesCounters.o
 
 clean : 
-	rm -f counter.out solo.out AriesCounters.o libariescounters.a
+	rm -f counter.out sample.out mpitest.out AriesCounters.o libariescounters.a
 	
---
+++ test3.cpp
@@ -0,0 +1,51 @@
+#include "AriesCounters.h"
+#include <unistd.h> // sleep
+#include <stdio.h>
+#include <time.h>
+
+#include "mpi.h"
+
+#define NANO_SECOND_MULTIPLIER  1000000  // 1 millisecond = 1,000,000 Nanoseconds
+
+int main(int argc, char *argv[])
+{
+	int numtasks, taskid;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+
+
+	int AC_event_set;
+	char** AC_events;
+	long long * AC_values;
+	int AC_event_count;
+
+	if (taskid%16 == 0)
+		StartRecordAriesCounters(&AC_event_set, &AC_events, &AC_values, &AC_event_count);
+	
+	// int i = 0;
+	// FILE* fp = fopen("sampleData.txt", "w"); // clear file
+	// fclose(fp);
+	// fp = fopen("sampleData.txt", "w+");
+
+	// struct timespec tim;
+	// tim.tv_sec = 0;
+	// tim.tv_nsec = 500*NANO_SECOND_MULTIPLIER;
+
+	// for (i = 0; i < 10; i++)
+	// {
+	// 	nanosleep(&tim, NULL);
+	// 	fprintf(fp,"Sample %d\n", i);
+	// 	SampleAriesCounters(fp, &AC_event_set, &AC_events, &AC_values, &AC_event_count);
+	// }
+	// fclose(fp);
+	
+	sleep(25);
+
+	if (taskid%16 == 0)
+		EndRecordAriesCounters(taskid, &AC_event_set, &AC_events, &AC_values, &AC_event_count);
+	
+	MPI_Finalize();
+
+	return 0;
+}
