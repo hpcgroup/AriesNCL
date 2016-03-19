fe33734e0eecf124303a15049076ae0879c554b4
Staci Smith <smith504@lc.llnl.gov>
2016-03-19 11:22:35 -0700

    Minor edit.

--- RecoverAriesCounters.c
+++ RecoverAriesCounters.c
@@ -1,3 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2014, Lawrence Livermore National Security, LLC.
+// Produced at the Lawrence Livermore National Laboratory.
+//
+// Written by:
+//     Dylan Wang <dylan.wang@gmail.com>
+//     Staci Smith <smiths949@email.arizona.edu>
+//     Abhinav Bhatele <bhatele@llnl.gov>
+//
+// LLNL-CODE-678960. All rights reserved.
+//
+// This file is part of AriesNCL. For details, see:
+// https://github.com/LLNL/ariesncl
+// Please also read the LICENSE file for our notice and the LGPL.
+//////////////////////////////////////////////////////////////////////////////
+
+
 #include <stdio.h>
 #include "AriesCounters.h"
 
